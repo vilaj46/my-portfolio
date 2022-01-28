0a0adab8b3dcae98af05ad14a89eaf865323dcbc
Julian <vilaj46@gmail.com>
2022-01-28 09:37:50 -0500

    Added underline to LOGO and hamburger animation / box shadow.

--- src/components/pages/Container.js
+++ src/components/pages/Container.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "gatsby";
-import Hamburger from "hamburger-react";
+import { Squash as Hamburger } from "hamburger-react";
 
 import Footer from "../footer/Footer";
 import HeaderNav from "./headerNav/HeaderNav";
@@ -10,14 +10,18 @@ export default function Container({ children }) {
   return (
     <div className="container mx-auto m-12 text-primary relative w-10/12 md:w-full">
       <HeaderNav isOpen={isOpen} />
-      <div className="absolute -top-0 right-0 z-50">
-        {isOpen && (
-          <Hamburger toggled={isOpen} toggle={setOpen} color="white" />
-        )}
-
-        {!isOpen && (
-          <Hamburger toggled={isOpen} toggle={setOpen} color="black" />
-        )}
+      <div
+        className="absolute -top-0 right-0 z-50"
+        style={{
+          boxShadow: `${isOpen ? "" : "1px 1px 3px 1px #333"}`,
+        }}
+      >
+        <Hamburger
+          toggled={isOpen}
+          toggle={setOpen}
+          color={isOpen ? "white" : "black"}
+          label="Show menu"
+        />
       </div>
       <div className="flex">
         <div
@@ -28,7 +32,10 @@ export default function Container({ children }) {
             ${isOpen ? "absolute" : "static"}
           `}
         >
-          <h1 className="mb-12 text-2xl" style={{ fontFamily: "inconsolata" }}>
+          <h1
+            className="mb-12 text-2xl hover:underline"
+            style={{ fontFamily: "inconsolata" }}
+          >
             <Link to="/">MyPortfolio</Link>
           </h1>
         </div>
--- src/data/index.js
+++ src/data/index.js
@@ -5,7 +5,7 @@ const data = {
   hero: "Julian Vila",
   hero_description: "Freelance Web Developer & Partner of Franklin Court Press",
   about_me:
-    "I'm Julian, an avid front-end developer who enjoys taking on new challenges and is always looking for ways to improve. I value critical thinking, punctuality and attention to detail. The journey from never writing a line of code to self-learning enough to be an employed web developer has been a crazy one, and i'm still planning on learning more!",
+    "I'm Julian, an avid web developer who enjoys taking on new challenges and is always looking for ways to improve. I value critical thinking, punctuality and attention to detail. The journey from never writing a line of code to self-learning enough to be an employed web developer has been a crazy one, and i'm still planning on learning more!",
 };
 
 export default data;
